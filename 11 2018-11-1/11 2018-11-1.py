ce2e1cf25137e09cebd7af46580dba1c096c3eda
thetime50 <1004763777@qq.com>
2018-11-03 16:22:40 +0800

    修改当前位置逻辑 修复速度计算bug

--- 11 2018-11-1/11 2018-11-1.py
+++ 11 2018-11-1/11 2018-11-1.py
@@ -19,10 +19,10 @@ Wind_h=550
 
 
 class OneDimensionalMoveClass:
-	def __init__(self, x, a, v0=0, vmax=0):
-		self._x0 = x
+	def __init__(self, x0, a, v0=0, vmax=0):
+		self._x0 = x0
 		self._v0 = v0
-		self._x = x
+		self._old_x = x0
 		self._a = a
 		self._vmax = vmax
 		self._current_vmax = vmax
@@ -30,7 +30,7 @@ class OneDimensionalMoveClass:
 		self._t1 = 0
 		self._t2 = 0
 		self._t3 = 0
-		self._des_x = x
+		self._des_x = x0
 		self._current_a = a
 		self._s0=0
 		self._s1=0
@@ -38,26 +38,30 @@ class OneDimensionalMoveClass:
 	def get_v(self,tim=0):
 		if tim==0:
 			tim=time.time()
-		if(abs(self._x-self._des_x)<0.000001):
-			return 0
-		if tim>self._tim0+self._t2:
+		if tim>self._tim0+self._t3:
+			v=0
+		elif tim>self._tim0+self._t2:
 			t=tim-self._tim0-self._t2
-			v= self._v0+self._current_a*t
+			v=self._current_vmax-self._current_a*t
 		elif tim > self._tim0 + self._t1:
 			v=self._current_vmax
 		else:
 			t = tim - self._tim0
-			v=self._current_vmax-self._current_a*t
+			v= self._v0+self._current_a*t
 		return v
 	def start_move_des(self, des_x):
+		dummy,x=self.get_x()
+		delta_x = des_x - x
+		if delta_x==0:
+			return
+
 		tim=time.time()
 		v=self.get_v(tim)
 		self._tim0 = tim
-		self._x0 = self._x
+		self._x0 = x
 		self._v0 = v
 
 		self._des_x = des_x
-		delta_x = des_x - self._x
 		self._current_a = math.copysign(self._a, delta_x)
 
 		current_vmax = (self._current_a * (delta_x) + 0.5 * v ** 2) ** 0.5
@@ -70,39 +74,42 @@ class OneDimensionalMoveClass:
 			self._t2=self._t1+(delta_x - (current_vmax**2-self._v0**2)/2/self._current_a - current_vmax**2/self._current_a/2)/current_vmax
 		self._current_vmax=current_vmax
 
+		'''
 		if abs(v - current_vmax) > 0.0001 and \
 				math.copysign(1, v) == math.copysign(1, current_vmax) and \
 				abs(v) > abs(current_vmax):
 			# 不接近 同号 当前速度更大
-			self._v0=self._v = current_vmax
+			self._v0 = current_vmax
 			self._t1=self._t2=0
+		'''
 
 		self._s0=(self._current_vmax**2-self._v0**2)/2/self._current_a
 		self._s1=(self._t2-self._t1)*self._current_vmax
 		self._t3=self._t2+(delta_x-self._s0-self._s1)/current_vmax*2
 
+		print(self._v0,current_vmax)
 	def start_move_delta(self, delta_x):
-		self._start_move_delta(self._x + delta_x)
+		self._start_move_delta(self.get_x() + delta_x)
 
-	def move_cb(self):
+	def get_x(self):
 		tim=time.time()
-		old_x=self._x
+		old_x=self._old_x
 		if(tim>= self._tim0+self._t3):
-			self._x=self._des_x
-			return old_x,self._x
+			self._old_x=self._des_x
+			return old_x,self._old_x
 		elif tim>self._tim0+self._t2:
 			t=tim-self._tim0-self._t2
 			delta_x=self._s0 + self._s1 + self._current_vmax*t-0.5*self._current_a*t**2
-			self._x=self._x0+delta_x
+			self._old_x=self._x0+delta_x
 		elif tim > self._tim0 + self._t1:
 			t=tim-self._tim0-self._t1
 			delta_x=self._s0 + t*self._current_vmax
-			self._x=self._x0+delta_x
+			self._old_x=self._x0+delta_x
 		else:
 			t = tim - self._tim0
 			delta_x=self._v0*t+0.5*self._current_a*t**2
-			self._x=self._x0+delta_x
-		return old_x,self._x
+			self._old_x=self._x0+delta_x
+		return old_x,self._old_x
 
 
 '''
@@ -154,7 +161,7 @@ root.resizable(False,False)
 cv0=tk_add_canvas(root,cv0_w)
 cv1=tk_add_canvas(root,cv1_w)
 
-od_move=OneDimensionalMoveClass(10,1000)
+od_move=OneDimensionalMoveClass(10,100)
 def draw_cv0_recttangle(cv,x,y,w,h,fill='black',width=0):
 	#print(x,y,w,h,fill,width)
 	if w>1:	w-=1
@@ -178,16 +185,16 @@ def cv0_b1_fun(event):
 	#draw_cv0_recttangle(cv0,event.x, cv0_w.h / 2, 2, cv0_w.h * 0.6, fill="red")
 
 def cv0_after_fun():
-	old_x,x=od_move.move_cb()
+	old_x,x=od_move.get_x()
 	if old_x!=x:
 		#draw_cv0_recttangle(cv0,old_x, cv0_w.h / 2, cv0_w.h*0.01, cv0_w.h * 0.6, fill="white")
 		#draw_cv0_recttangle(cv0,x, cv0_w.h / 2, cv0_w.h*0.01, cv0_w.h * 0.6, fill="black")
 		change_recttangle(cv0,rect0,x, cv0_w.h / 2, cv0_w.h*0.01, cv0_w.h * 0.6)
 		#print(x)
-	cv0.after(20, cv0_after_fun)
+	cv0.after(100, cv0_after_fun)
 cv0.bind('<B1-Motion>',cv0_b1_fun)
 cv0.bind('<Button-1>',cv0_b1_fun)
-cv0.after(20, cv0_after_fun)
+cv0.after(100, cv0_after_fun)
 
 
 root.mainloop()
